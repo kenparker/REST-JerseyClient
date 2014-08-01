93075fb1784dd3d7985099a6506e42d4421a3f49
ken <magangde@yahoo.de>
2014-08-01 09:45:05 +0200

    Base version (working)

---
+++ src/main/java/com/maggioni/jersey/client/HelloClientMain.java
@@ -0,0 +1,32 @@
+
+package com.maggioni.jersey.client;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+
+
+
+/**
+ *
+ * @author maggioni
+ */
+public class HelloClientMain {
+
+    private static final String BASE_URI = "http://localhost:8080/REST-MyFirstRest/resources";
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Client client = ClientBuilder.newClient();
+        WebTarget res = client.target(BASE_URI).path("hello");
+        String get = res.request(MediaType.TEXT_XML).get(String.class);
+        System.out.println(get);
+        client.close();
+        
+    }
+    
+}
